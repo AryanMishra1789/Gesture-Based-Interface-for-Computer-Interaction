345feba71016ce4df064e36f3598b72e8118aa52
Aryan Mishra <158508418+AryanMishra1789@users.noreply.github.com>
2025-02-22 21:32:18 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,10 +12,10 @@ echo "\
 export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/usr/lib/x86_64-linux-gnu\n\
 " >> ~/.profile
 
+source ~/.profile
+
 # Start Xvfb
 Xvfb :99 -screen 0 1024x768x16 &
 
 # Set the DISPLAY environment variable
 export DISPLAY=:99
-
-source ~/.profile
